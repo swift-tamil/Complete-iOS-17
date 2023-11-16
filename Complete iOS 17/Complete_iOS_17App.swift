3cc2cfe0f11a3705695aad1b43cc6064315c8944
MohamedGoush <0x1110111@gmail.com>
2023-11-16 22:35:04 +0530

    Initial Commit

---
+++ Complete iOS 17/Complete_iOS_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Complete_iOS_17App.swift
+//  Complete iOS 17
+//
+//  Created by python on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Complete_iOS_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Complete iOS 17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Complete iOS 17
+//
+//  Created by python on 16/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
